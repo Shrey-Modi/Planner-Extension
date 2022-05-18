933b2c65a5f409924a7d8ad4f3ad443952e4b3da
Shrey-Modi <shreymodi18@gmail.com>
2022-05-18 14:30:53 +0530

    added basic frontend popup

---
+++ main_script.js
@@ -0,0 +1,9 @@
+function start(){
+    var button = document.getElementById("main");
+    var text = document.getElementById("text");
+
+    button.disabled = true;
+    button.innerHTML = "working"
+    text.innerHTML = "pls wait..."
+
+}
