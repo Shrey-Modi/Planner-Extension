c2248a0d54fc721538189757f01feba401f00555
Shrey-Modi <shreymodi18@gmail.com>
2022-05-22 18:28:10 +0530

    figuring out how to get active tabs DOM

--- main_script.js
+++ main_script.js
@@ -5,5 +5,35 @@ function start(){
     button.disabled = true;
     button.innerHTML = "working"
     text.innerHTML = "pls wait..."
+    
+    // //get the active tab document and store it in a variable
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //     var tab = tabs[0];
+    //     var url = tab.url;
+    //     var title = tab.title;
+    // });
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {text : "report_back"}, getDOM);
+      });
+
+    // let alldays=dom.querySelectorAll("#tt-dayTitle");
+    // for(let i=0;i<alldays.length;i++){
+    //     alldays[i].style.backgroundColor="red";}
+    
+    //restore button and text
+    button.disabled = false;
+    button.innerHTML = "start"
+    text.innerHTML = "click to start"
 
 }
+function getDOM(response){
+    var alldays=response.querySelectorAll("#tt-dayTitle");
+    for(let i=0;i<alldays.length;i++){
+        alldays[i].style.backgroundColor="red";
+    }
+    
+}
+// console.log(document.querySelector('#main'));
+// document.querySelector('#main').addEventListener('click', start);
+document.getElementById("main").addEventListener("click", start);
