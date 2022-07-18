21dcab7b65bc07db84e427f3025cc6111e46b272
Shrey-Modi <shreymodi18@gmail.com>
2022-07-18 22:27:54 +0530

    Works with recurring events and end date
    a working version
    need to be cleaned up and resubmitted for review

--- inject.js
+++ inject.js
@@ -50,8 +50,9 @@ var ics = function(uidDomain, prodId) {
        * @param  {string} location    Location of event
        * @param  {string} begin       Beginning date of event
        * @param  {string} stop        Ending date of event
+       * @param  {string} until       repeat until this date(custom by me)
        */
-      'addEvent': function(subject, description, location, begin, stop, rrule) {
+      'addEvent': function(subject, description, location, begin, stop, until) {
         // I'm not in the mood to make these optional... So they are all required
         if (typeof subject === 'undefined' ||
           typeof description === 'undefined' ||
@@ -60,54 +61,6 @@ var ics = function(uidDomain, prodId) {
           typeof stop === 'undefined'
         ) {
           return false;
-        }
-  
-        // validate rrule
-        if (rrule) {
-          if (!rrule.rrule) {
-            if (rrule.freq !== 'YEARLY' && rrule.freq !== 'MONTHLY' && rrule.freq !== 'WEEKLY' && rrule.freq !== 'DAILY') {
-              throw "Recurrence rrule frequency must be provided and be one of the following: 'YEARLY', 'MONTHLY', 'WEEKLY', or 'DAILY'";
-            }
-  
-            if (rrule.until) {
-              if (isNaN(Date.parse(rrule.until))) {
-                throw "Recurrence rrule 'until' must be a valid date string";
-              }
-            }
-  
-            if (rrule.interval) {
-              if (isNaN(parseInt(rrule.interval))) {
-                throw "Recurrence rrule 'interval' must be an integer";
-              }
-            }
-  
-            if (rrule.count) {
-              if (isNaN(parseInt(rrule.count))) {
-                throw "Recurrence rrule 'count' must be an integer";
-              }
-            }
-  
-            if (typeof rrule.byday !== 'undefined') {
-              if ((Object.prototype.toString.call(rrule.byday) !== '[object Array]')) {
-                throw "Recurrence rrule 'byday' must be an array";
-              }
-  
-              if (rrule.byday.length > 7) {
-                throw "Recurrence rrule 'byday' array must not be longer than the 7 days in a week";
-              }
-  
-              // Filter any possible repeats
-              rrule.byday = rrule.byday.filter(function(elem, pos) {
-                return rrule.byday.indexOf(elem) == pos;
-              });
-  
-              for (var d in rrule.byday) {
-                if (BYDAY_VALUES.indexOf(rrule.byday[d]) < 0) {
-                  throw "Recurrence rrule 'byday' values must include only the following: 'SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'";
-                }
-              }
-            }
-          }
         }
   
         //TODO add time and time zone? use moment to format?
@@ -149,33 +102,6 @@ var ics = function(uidDomain, prodId) {
         var end = end_year + end_month + end_day + end_time;
         var now = now_year + now_month + now_day + now_time;
   
-        // recurrence rrule vars
-        var rruleString;
-        if (rrule) {
-          if (rrule.rrule) {
-            rruleString = rrule.rrule;
-          } else {
-            rruleString = 'rrule:FREQ=' + rrule.freq;
-  
-            if (rrule.until) {
-              var uDate = new Date(Date.parse(rrule.until)).toISOString();
-              rruleString += ';UNTIL=' + uDate.substring(0, uDate.length - 13).replace(/[-]/g, '') + '000000Z';
-            }
-  
-            if (rrule.interval) {
-              rruleString += ';INTERVAL=' + rrule.interval;
-            }
-  
-            if (rrule.count) {
-              rruleString += ';COUNT=' + rrule.count;
-            }
-  
-            if (rrule.byday && rrule.byday.length > 0) {
-              rruleString += ';BYDAY=' + rrule.byday.join(',');
-            }
-          }
-        }
-  
         var stamp = new Date().toISOString();
   
         var calendarEvent = [
@@ -188,14 +114,21 @@ var ics = function(uidDomain, prodId) {
           'DTEND;VALUE=DATE-TIME:' + end,
           'LOCATION:' + location,
           'SUMMARY;LANGUAGE=en-us:' + subject,
-          'TRANSP:TRANSPARENT',
+          'TRANSP:OPAQUE',
           'END:VEVENT'
         ];
   
-        if (rruleString) {
             var daysOfWeek = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA']
-          calendarEvent.splice(4, 0, "RRULE:FREQ=WEEKLY;WKST=SU;COUNT=15;BYDAY="+daysOfWeek[start_date.getDay()]);//test manual insert
-        }
+            var until_string = "";
+            var tempString = until.toISOString();
+            for (var i = 0; i < tempString.length; i++) {
+                if(!" -:.".includes(tempString[i])){
+                    until_string=until_string+tempString[i]
+                }
+            }
+            until_string = until_string.substring(0, until_string.length - 4);
+            until_string = until_string + "Z";
+          calendarEvent.splice(4, 0, "RRULE:FREQ=WEEKLY;WKST=SU;UNTIL="+until_string+";BYDAY="+daysOfWeek[start_date.getDay()]);//test manual insert
   
         calendarEvent = calendarEvent.join(SEPARATOR);
   
@@ -249,7 +182,6 @@ var ics = function(uidDomain, prodId) {
 var allClasses = document.getElementsByClassName("tt-activity");
 
 var cal = ics();
-
 for (let i = 0; i < allClasses.length; i++) {
 
     var className = allClasses[i].children[0].children[0].innerHTML;
@@ -262,6 +194,10 @@ for (let i = 0; i < allClasses.length; i++) {
     var endDate = time[1] + " " + date[0] + " " + date[1] + " " + date[date.length - 1];   
     var day = allClasses[i].parentElement.children[0].innerHTML.substring(0, 2).toUpperCase();
 
+    var untilDate = new Date(date[3]+" "+date[4] +" "+ date[date.length - 1])
+    // console.log(untilDate.toISOString());
+
+
 
     var startDateO = new Date(startDate);
     var endDateO = new Date(endDate);
@@ -273,16 +209,18 @@ for (let i = 0; i < allClasses.length; i++) {
     }
 
     endDateO.setDate(endDateO.getDate()- dayDiff);
-    endDate = endDateO.toISOString
+    endDate = endDateO.toISOString();
     startDateO.setDate(startDateO.getDate() - dayDiff);
     startDate = startDateO.toISOString();
 
     //trying to get rrule to work
 
     //repeat weekly for 15 counts
-    rrule_actual = {rrule:{freq:"WEEKLY",count:15}};
+    // rrule_actual = {rrule:{freq:"WEEKLY",count:15}};
 
-    cal.addEvent(className, description,description, startDate, endDate, rrule_actual);
+    // console.log(endDate);
+
+    cal.addEvent(className, description,description, startDate, endDate, untilDate);
 
     // this is from the main branch
     
